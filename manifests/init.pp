ff395ceccfbfeaa644444c65288a3429a0cc99e9
Hannes Doyle <hannesdoyle@gmail.com>
2015-10-30 14:52:01 +0100

    c&p error...

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,6 +30,55 @@ class network {
       fail('This network module only supports RedHat-based systems.')
     }
   }
+  # interface static
+  $network_if_statics = hiera_hash('network::if::static',undef)
+  if $network_if_statics != undef {
+    create_resources('network::if::static', $network_if_statics)
+  }
+
+  # interface dhcp
+  $network_if_dynamics = hiera_hash('network::if::dynamic',undef)
+  if $network_if_dynamics != undef {
+    create_resources('network::if::dynamic', $network_if_dynamics)
+  }
+
+  # interface aliases
+  $network_if_aliases = hiera_hash('network::if::alias',undef)
+  if $network_if_aliases != undef {
+    create_resources('network::if::alias', $network_if_aliases)
+  }
+
+  # bond static
+  $network_bond_statics = hiera_hash('network::bond::static',undef)
+  if $network_bond_statics != undef {
+    create_resources('network::bond::static', $network_bond_statics)
+  }
+
+  # bond dhcp
+  $network_bond_dynamics = hiera_hash('network::bond::dynamic',undef)
+  if $network_bond_dynamics != undef {
+    create_resources('network::bond::dynamic', $network_bond_dynamics)
+  }
+
+  # bond slaves
+  $network_bond_slaves = hiera_hash('network::bond::slave',undef)
+  if $network_bond_slaves != undef {
+    create_resources('network::bond::slave', $network_bond_slaves)
+  }
+
+  # bond aliases
+  $network_bond_aliases = hiera_hash('network::bond::alias',undef)
+  if $network_bond_aliases != undef {
+    create_resources('network::bond::alias', $network_bond_aliases)
+  }
+
+  # routes
+  $network_routes = hiera_hash('network::route',undef)
+  if $network_routes != undef {
+    create_resources('network::route', $network_routes)
+  }
+
+
 
   service { 'network':
     ensure     => 'running',
@@ -117,53 +166,6 @@ define network_if_base (
   $bridge = undef,
   $linkdelay = undef
 ) {
-  # interface static
-  $network_if_statics = hiera_hash('network::if::static',undef)
-  if $network_if_statics != undef {
-    create_resources('network::if::static', $network_if_statics)
-  }
-
-  # interface dhcp
-  $network_if_dynamics = hiera_hash('network::if::dynamic',undef)
-  if $network_if_dynamics != undef {
-    create_resources('network::if::dynamic', $network_if_dynamics)
-  }
-
-  # interface aliases
-  $network_if_aliases = hiera_hash('network::if::alias',undef)
-  if $network_if_aliases != undef {
-    create_resources('network::if::alias', $network_if_aliases)
-  }
-
-  # bond static
-  $network_bond_statics = hiera_hash('network::bond::static',undef)
-  if $network_bond_statics != undef {
-    create_resources('network::bond::static', $network_bond_statics)
-  }
-
-  # bond dhcp
-  $network_bond_dynamics = hiera_hash('network::bond::dynamic',undef)
-  if $network_bond_dynamics != undef {
-    create_resources('network::bond::dynamic', $network_bond_dynamics)
-  }
-
-  # bond slaves
-  $network_bond_slaves = hiera_hash('network::bond::slave',undef)
-  if $network_bond_slaves != undef {
-    create_resources('network::bond::slave', $network_bond_slaves)
-  }
-
-  # bond aliases
-  $network_bond_aliases = hiera_hash('network::bond::alias',undef)
-  if $network_bond_aliases != undef {
-    create_resources('network::bond::alias', $network_bond_aliases)
-  }
-
-  # routes
-  $network_routes = hiera_hash('network::route',undef)
-  if $network_routes != undef {
-    create_resources('network::route', $network_routes)
-  }
 
   # Validate our booleans
   validate_bool($userctl)
